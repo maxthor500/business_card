4b545f18a3ceb9dff6be8e5ede186cb904ea97b3
maxthor500 <iraubuntu@gmail.com>
2022-01-31 23:25:14 +0000

    Complete the style of the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,8 +8,27 @@ export default function Card() {
             <img className="card-img-top" src={cartoon} alt="My portrait" />
             <div className="card-body">
                 <h2 className="card-title">Carmine Pastena</h2>
-                <p className="card-text">Text</p>
-                {/* <a href="#" className="btn">Go somewhere</a> */}
+                <h5 className="card-text">Full-Stack Web Developer</h5>
+                <div className="container-buttons">
+                    <a href="mailto:carmine_pastena@alice.it" className="btn btn-email"><i className="fas fa-envelope"></i>Email</a>
+                    <a href="https://www.linkedin.com/in/carmine-pastena-a34b4654/" className="btn btn-in"><i className="fab fa-linkedin"></i>LinkedIn</a>
+                </div>
+                <div className="container">
+                    <h4 className="div-title">About</h4>
+                    <p className="div-text">
+                        I am a Full-Stack Web Developer with an interest of the variuos pragramming
+                        languages and I am always happy to learn new things. 
+                        My skills are HTML, CSS, JS that I use for the frontend and am learning React,
+                        in the backend I use MySQL and MongoDB as Database and Python as language 
+                        to build web app with Django and Flask.
+                    </p>
+                </div>
+                <div className="container">
+                    <h4 className="div-title">Interests</h4>
+                    <p className="div-text">
+                        Reader, Aikidoka, Trekking, Travel, Cooking Italian dishes.
+                    </p>
+                </div>
             </div>
             <Footer />
         </div>
